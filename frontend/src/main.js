59d5375ee66b183aa9531f57d1043548d9d6f5d2
ElvisAns <ansimapersic@gmail.com>
2022-08-20 21:42:46 +0200

    Add bs icons to app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createApp(App).use(store).use(router).mount("#app");
