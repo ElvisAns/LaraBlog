08bcd848e82ee7a5884ce26ed754ac98b77f5fda
ElvisAns <ansimapersic@gmail.com>
2022-08-20 14:13:21 +0200

    Basic routes endpoint for posts

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/posts/{limit?}',[PostController::class,'index']);
+Route::get('/posts/read/{slug}',[PostController::class,'read']);
+
+Route::post('/posts/create',[PostController::class,'create']);
+Route::post('/posts/update/{post_id}',[PostController::class,'update']);
+
+Route::get('/posts/delete/{post_id}')
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
