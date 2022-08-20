d437c703e525ca22196d47acfecb5e0672a26acc
ElvisAns <ansimapersic@gmail.com>
2022-08-20 23:20:26 +0200

    Imported axios and axios-vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,5 +5,7 @@ import store from "./store";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(VueAxios, axios).mount("#app");
