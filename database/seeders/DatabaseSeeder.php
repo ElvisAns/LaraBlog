2ed5cc863248d5f255f7b3af0c3bc650733c2df5
ElvisAns <ansimapersic@gmail.com>
2022-08-20 09:54:26 +0200

    Wrtitting the database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,55 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+use App\Models\User;
+use App\Models\Posts;
+use App\Models\Comments;
+use App\Models\Categories;
+
+use Faker\Factory as Faker;
+
+use Carbon\Carbon;
+
+trait varSet {
+    public static $user;
+    public static $category;
+    public static $post;
+    public static $curr_timestamp;
+    public static $fake_datas;
+}
+
+$category=1;
 
 class DatabaseSeeder extends Seeder
 {
+    use varSet;
     /**
      * Seed the application's database.
      *
      * @return void
      */
+    public function __construct(){
+        varSet::$curr_timestamp = Carbon::now()->timestamp;
+        varSet::$fake_datas=Faker::create();
+    }
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([Users_table_seeder::class,Categories_table_seeder::class,Post_table_seeder::class,Comment_table_seeder::class]);
+ 
+        $this->command->info('Dummy datas has been inserted successfully');
+        $this->command->info("Please login using johndoe@blog.com & 12345678 as password");
     }
 }
 
 class Users_table_seeder extends Seeder
 {
+    use varSet;
     /**
      * Run the database seeds.
      *
@@ -26,12 +59,24 @@ class Users_table_seeder extends Seeder
      */
     public function run()
     {
-        //
+        varSet::$user = User::create([
+            'email'=>'johndoe@blog.com',
+            'name'=>'John Doe',
+            'password'=>Hash::make('12345678')
+        ]);
     }
 }
-
+class Categories_table_seeder extends Seeder{
+    public function run(){
+        varSet::$category = Categories::create([
+            'name' => "Dummy posts",
+            'details' => "Posts with mock datas"
+        ]);
+    }
+}
 class Post_table_seeder extends Seeder
 {
+    use varSet;
     /**
      * Run the database seeds.
      *
@@ -39,13 +84,24 @@ class Post_table_seeder extends Seeder
      */
     public function run()
     {
-        //
+
+        varSet::$post = Posts::create([
+            'title' => Str::random(10),
+            'caption' => "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            'content' => $content,
+            'image_url' => "https://www.maxitendance.com/wp-content/uploads/2021/09/doodle-ipsum-lorem-ipsum-illustration-01.jpg",
+            'category' => varSet::$category->id,
+            'user_id' => varSet::$user->id,
+            'created_at' => varSet::$curr_timestamp,
+            'updated_at' => varSet::$curr_timestamp
+        ]);
     }
 }
 
 
 class Comment_table_seeder extends Seeder
 {
+    use varSet;
     /**
      * Run the database seeds.
      *
@@ -53,6 +109,12 @@ class Comment_table_seeder extends Seeder
      */
     public function run()
     {
-        //
+        Comments::create([
+            'comment' => 'The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters',
+            'user_id' => varSet::$user->id,
+            'post_id' => varSet::$post->id,
+            'created_at' => varSet::$curr_timestamp,
+            'updated_at' => varSet::$curr_timestamp
+        ]);
     }
 }
