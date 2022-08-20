1f9335dc8a158f57301b434ebfde943e54fc430b
ElvisAns <ansimapersic@gmail.com>
2022-08-20 14:12:07 +0200

    Post Controller CRUD methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Posts;
+use App\Models\Comments;
 
 use Carbon\Carbon;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -14,9 +18,14 @@ class PostController extends Controller
         return $posts;
     }
 
-    public function read($title){
-        $post = Posts::where("title",$title)->first();
-        return $post;
+    public function read($slug){
+        $post_title = Str::replace('-'," ",$slug);
+        $post = Posts::where("title",$post_title)->first();
+        if($post==NULL){
+            return response(["response"=>"Post not found"],404);
+        }
+        $response = ["post"=>$post,"comments"=>$post->comments->count()];
+        return $response;
     }
 
     public function create(Request $request){
@@ -29,20 +38,32 @@ class PostController extends Controller
         $post->user_id=$request->input('user_id');
         $post->created_at=Carbon::now()->timestamp;
         $post->updated_at=Carbon::now()->timestamp;
-        $post->save();
-        return ['reponse'=>'Post Created'];
+        try{
+            $post->save();
+            return ['reponse'=>'Post Created'];
+        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            return response(['reponse'=>'Error creating new post'],400);
+        }
     }
 
     public function update($post_id,Request $request){
         $post = Posts::find($post_id);
-        $post->title=$request->input('title');
-        $post->caption=$request->input('caption');
-        $post->content=$request->input('content');
-        $post->image_url=$request->input('image_url');
-        $post->category=$request->input('category');
-        $post->user_id=$request->input('user_id');
         $post->updated_at=Carbon::now()->timestamp;
-        $post->update();
-        return ['reponse'=>'Post Updated'];
+        try{
+            $post->update($request->all());
+            return ['reponse'=>'Post Updated'];
+        }
+        catch(\Exception $e){
+            Log::error($e);
+            return response(['reponse'=>'Error updating post'],400);
+        }
+    }
+
+    public function delete($post_id){
+        $post = Posts::find($post_id);
+        $post->delete();
     }
 }
