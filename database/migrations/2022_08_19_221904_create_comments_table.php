bf89363144a564bd27009f1b3d25d12ce1f8b081
ElvisAns <ansimapersic@gmail.com>
2022-08-20 07:55:58 +0200

    Add seeders for tables

--- database/migrations/2022_08_19_221904_create_comments_table.php
+++ database/migrations/2022_08_19_221904_create_comments_table.php
@@ -15,8 +15,6 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->integer('post_id');
             $table->string('comment');
             $table->timestamps();
 
---
+++ database/seeders/Users_table_seeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class Users_table_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
