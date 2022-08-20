27fd953f20fec4db7e103296d8a2530e4cdef2e2
ElvisAns <ansimapersic@gmail.com>
2022-08-20 15:36:27 +0200

    Usefull Api routes definition

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,11 +17,12 @@ use App\Http\Controllers\PostController;
 */
 Route::get('/posts/{limit?}',[PostController::class,'index']);
 Route::get('/posts/read/{slug}',[PostController::class,'read']);
-
+Route::get('/posts/delete/{post_id}',[PostController::class,'destroy']);
 Route::post('/posts/create',[PostController::class,'create']);
 Route::post('/posts/update/{post_id}',[PostController::class,'update']);
 
-Route::get('/posts/delete/{post_id}')
+Route::get('/users/get/{user_id}',[UserController::class,'get']);
+Route::get('/users/list/all',[UserController::class,'all']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
