41dd32e7fec2dd555b537987376081ca4dd9b188
ElvisAns <ansimapersic@gmail.com>
2022-08-20 08:34:03 +0200

    Add user_id coloumn to post table

---
+++ database/migrations/2022_08_20_062616_add_user_id_column_to_posts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdColumnToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->addColumn('Integer','user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->dropColumn('user_id');
+        });
+    }
+}
