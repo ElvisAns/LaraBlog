1135fb79eb9587e784229a841751bc33292e4fd7
ElvisAns <ansimapersic@gmail.com>
2022-08-20 15:35:30 +0200

    Added necessary methods to controllers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Comments;
+
 class CommentController extends Controller
 {
-    //
+    public function destroy($comment_id){
+        $comment = Comments::find($comment_id);
+        if($post){
+            $comment->delete();
+            return ['reponse'=>'Comment successfully deleted'];
+        }
+        else{
+            return response(['response'=>'Error deleting comment,Comment don\'t exist'],404);
+        }
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
 
 class UserController extends Controller
 {
-    //
+    public function get($user_id){
+        $users = User::find($user_id);
+        if($users)
+            return $users;
+        else
+            return response(["response"=>"No user found"],404);
+    }
+
+    public function all(){
+        $users = User::all();
+        return $users;
+    }
+
+    public function create(Request $request){
+        $user = new User();
+        $post->name=$request->input('name');
+        $post->email=$request->input('email');
+        $post->password=Hash::make($request->input('password'));
+        $post->created_at=Carbon::now()->timestamp;
+        $post->updated_at=Carbon::now()->timestamp;
+        try{
+            $post->save();
+            return ['reponse'=>'User Created'];
+        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            return response(['reponse'=>'Error creating new user'],400);
+        }
+    }
+    
 }
