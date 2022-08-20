9fd658e353401a035d8246557e2b86c7d26cd3fd
ElvisAns <ansimapersic@gmail.com>
2022-08-20 11:24:43 +0200

    Buidling Post controller [Added update method]

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Posts;
 
+use Carbon\Carbon;
+
 class PostController extends Controller
 {
     public function index($limit=null){
@@ -20,12 +22,27 @@ class PostController extends Controller
     public function create(Request $request){
         $post = new Posts();
         $post->title=$request->input('title');
-        $post->caption=$request->input('');
-        $post->content=$request->input('');
-        $post->image_url=$request->input('');
-        $post->category=$request->input('');
-        $post->user_id=$request->input('');
-        $post->created_at=$request->input('');
-        $post->updated_at=$request->input('');
+        $post->caption=$request->input('caption');
+        $post->content=$request->input('content');
+        $post->image_url=$request->input('image_url');
+        $post->category=$request->input('category');
+        $post->user_id=$request->input('user_id');
+        $post->created_at=Carbon::now()->timestamp;
+        $post->updated_at=Carbon::now()->timestamp;
+        $post->save();
+        return ['reponse'=>'Post Created'];
+    }
+
+    public function update($post_id,Request $request){
+        $post = Posts::find($post_id);
+        $post->title=$request->input('title');
+        $post->caption=$request->input('caption');
+        $post->content=$request->input('content');
+        $post->image_url=$request->input('image_url');
+        $post->category=$request->input('category');
+        $post->user_id=$request->input('user_id');
+        $post->updated_at=Carbon::now()->timestamp;
+        $post->update();
+        return ['reponse'=>'Post Updated'];
     }
 }
