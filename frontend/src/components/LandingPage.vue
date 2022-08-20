691adbdfcc2573b4c7bedf2843ba383ccffcdd34
ElvisAns <ansimapersic@gmail.com>
2022-08-20 21:58:54 +0200

    Component factory (landing for now)

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -1,9 +1,19 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-    <div class="container">
-        <div class="landing">
-            <h1>Welcome To Blog</h1>
-            <p>We usually publish great news here, make sure you are tuned in!</p>
+    <div class="landing">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-white">
+                        <h1>Welcome Our To Blog</h1>
+                        <button class="btn btn-blue btn-space">GET STARTED</button>
+                        <p  class="text-secondary">
+                            We usually publish great news here, make sure you are tuned in!
+                            We usually publish great news here, make sure you are tuned in!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -18,12 +28,26 @@ export default {
 
 <style scoped>
 .landing {
-    height: 100%;
+    height: 88%;
     width: 100%;
     background-color: rgb(33, 37, 41);
 }
 .landing {
     display: grid;
     place-items: center;
+}
+.btn-blue {
+    background-color: rgb(59 151 246);
+    color: white;
+}
+.btn-space {
+    padding: 10px 40px;
+    margin: 50px 50px;
+}
+.landing {
+    background-image: radial-gradient(rgb(134, 134, 134) 1px, transparent 0);
+    background-size: 40px 40px;
+    background-position: -50% -19px;
+    clip-path: polygon(0 0, 100% 0%, 100% 91%, 55% 98%, 0 100%);
 }
 </style>
