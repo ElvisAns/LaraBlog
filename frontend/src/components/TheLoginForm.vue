9b86d0a0ed89fd192b4c7fcf8ac616c10b9c9734
ElvisAns <ansimapersic@gmail.com>
2022-08-21 14:07:58 +0200

    Some update on the login form

--- frontend/src/components/TheLoginForm.vue
+++ frontend/src/components/TheLoginForm.vue
@@ -1,40 +1,39 @@
 <template>
     <form>
-        <!-- Email input -->
         <div class="form-outline mb-4">
-          <input type="email" id="form1Example1" class="form-control" />
-          <label class="form-label" for="form1Example1">Email address</label>
+            <input type="email" id="form1Example1" class="form-control" />
+            <label class="form-label" for="form1Example1">Email address</label>
         </div>
-      
-        <!-- Password input -->
         <div class="form-outline mb-4">
-          <input type="password" id="form1Example2" class="form-control" />
-          <label class="form-label" for="form1Example2">Password</label>
+            <input type="password" id="form1Example2" class="form-control" />
+            <label class="form-label" for="form1Example2">Password</label>
         </div>
-      
-        <!-- 2 column grid layout for inline styling -->
         <div class="row mb-4">
-          <div class="col d-flex justify-content-center">
-            <!-- Checkbox -->
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-              <label class="form-check-label" for="form1Example3"> Remember me </label>
+            <div class="col d-flex justify-content-center">
+                <div class="form-check">
+                    <input
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="form1Example3"
+                        checked
+                    />
+                    <label class="form-check-label" for="form1Example3">
+                        Remember me
+                    </label>
+                </div>
             </div>
-          </div>
-      
-          <div class="col">
-            <!-- Simple link -->
-            <a href="#!">Forgot password?</a>
-          </div>
+
+            <div class="col">
+                <a href="#!">Forgot password?</a>
+            </div>
         </div>
-      
-        <!-- Submit button -->
         <button type="submit" class="btn btn-primary btn-block">Sign in</button>
     </form>
 </template>
 
 <script>
-    export default{
-        name:"TheLoginForm"
-    }
+export default {
+    name: "TheLoginForm",
+};
 </script>
