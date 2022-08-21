8edc8c56caf6cac736edd3cfd4ae55368904675f
ElvisAns <ansimapersic@gmail.com>
2022-08-21 11:00:33 +0200

    The usermanager component factor

--- frontend/src/components/TheUserManager.vue
+++ frontend/src/components/TheUserManager.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="d-flex mx-auto justify-content-center">
-        <router-link to="/login" class="btn btn-success mx-1 btn-md px-4">
-            login
-        </router-link>
-        <router-link to="/signin" class="btn btn-light mx-1 btn-md px-4">
-            register
-        </router-link>
+    <div v-show="!userIsLoggedIn">
+        <div class="d-flex mx-auto justify-content-center">
+            <router-link
+                to="auth/login"
+                class="btn btn-success mx-1 btn-md px-4"
+            >
+                login
+            </router-link>
+            <router-link
+                to="auth/signin"
+                class="btn btn-light mx-1 btn-md px-4"
+            >
+                register
+            </router-link>
+        </div>
+    </div>
+    <div v-show="userIsLoggedIn">
+        <div class="d-flex mx-auto justify-content-center">
+            <router-link to="auth/logout" class="btn btn-success mx-1 btn-md px-4">
+                logout
+            </router-link>
+            <router-link to="/signin" class="btn btn-light mx-1 btn-md px-4">
+                profile
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -13,9 +31,10 @@
 export default {
     name: "TheUserManager",
     props: {
-        userLogin: {
+        userIsLoggedIn: {
             required: true,
             type: Boolean,
+            default: false,
         },
     },
 };
