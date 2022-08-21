35ea727a9e87f6f5bc840485823b9b526b8a9c60
ElvisAns <ansimapersic@gmail.com>
2022-08-21 19:03:40 +0200

    The post viewer component factory

--- frontend/src/components/ThePostViewer.vue
+++ frontend/src/components/ThePostViewer.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="container">
         <div class="row">
-            <article
-                class="col-md-8 offset-md-2 mt-5 bg-dark"
-                :ref="blog_container"
-            >
-            <div v-show="data_fetched">
-                <h1>{{ post.title }}</h1>
-            </div>
+            <article class="col-md-8 offset-md-2 mt-5" ref="blog_container">
+                <div
+                    v-show="error.length & data_fetched"
+                    class="alert alert-danger d-flex align-items-center"
+                    role="alert"
+                >
+                    <svg
+                        class="bi flex-shrink-0 me-2"
+                        width="24"
+                        height="24"
+                        role="img"
+                        aria-label="Danger:"
+                    >
+                        <use xlink:href="#exclamation-triangle-fill" />
+                    </svg>
+                    <div>{{ error }}</div>
+                </div>
+                <div v-show="data_fetched">
+                    <img :src="post.image_url" />
+                    <h1>{{ post.title }}</h1>
+                </div>
             </article>
         </div>
     </div>
@@ -25,12 +39,14 @@ export default {
     data() {
         return {
             post: {},
-            data_fetched:false,
+            data_fetched: false,
+            error: "",
         };
     },
     mounted() {
+        console.log(this.$refs);
         let loader = this.$loading.show({
-            container: this.$ref.blog_container,
+            container: this.$refs.blog_container,
             canCancel: false,
         });
 
@@ -40,9 +56,17 @@ export default {
             )
             .then((res) => {
                 this.post = res.data;
+                this.data_fetched = true;
                 setTimeout(() => {
                     loader.hide();
                 }, 1000);
+            })
+            // eslint-disable-next-line no-unused-vars
+            .catch((err) => {
+                this.error =
+                    "Error occured while fetching the post, seems like the post doesnt exist";
+                this.data_fetched = true;
+                loader.hide();
             });
     },
 };
