4b3a55df7b24acabd9610f43b8357ab3d6ba6a70
ElvisAns <ansimapersic@gmail.com>
2022-08-21 17:13:05 +0200

    Removed bootsrap validation logic

--- frontend/src/components/TheLoginForm.vue
+++ frontend/src/components/TheLoginForm.vue
@@ -90,24 +90,6 @@ export default {
             submit_disabled: false,
         };
     },
-    mounted() {
-        var forms = document.querySelectorAll(".needs-validation");
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms).forEach(function (form) {
-            form.addEventListener(
-                "submit",
-                function (event) {
-                    if (!form.checkValidity()) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-
-                    form.classList.add("was-validated");
-                },
-                false
-            );
-        });
-    },
     methods: {
         login(event) {
             const toast = useToast();
