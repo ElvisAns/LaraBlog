dffd5cf9243ad5c938e7eb984b6675da93d07e86
ElvisAns <ansimapersic@gmail.com>
2022-08-21 18:42:25 +0200

    The post viewer component factory

--- frontend/src/components/ThePostViewer.vue
+++ frontend/src/components/ThePostViewer.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container">
         <div class="row">
-            <article class="col-md-10 offset-md-1"></article>
+            <article
+                class="col-md-8 offset-md-2 mt-5 bg-dark"
+                :ref="blog_container"
+            >
+                {{ slug }}
+            </article>
         </div>
     </div>
 </template>
@@ -9,5 +14,28 @@
 <script>
 export default {
     name: "ThePostViewer",
+    props: {
+        slug: {
+            required: true,
+            type: String,
+        },
+    },
+    
+    mounted() {
+        let loader = this.$loading.show({
+            container: this.$ref.blog_container,
+            canCancel: false,
+        });
+
+        this.axios
+            .get(
+                `${process.env.VUE_APP_BACKEND_BASE_URL}/post/read/${this.slug}`
+            )
+            .then((res) => {
+                setTimeout(() => {
+                    loader.hide();
+                }, 1000);
+            });
+    },
 };
 </script>
