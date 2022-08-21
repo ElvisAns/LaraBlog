1ef4ef7a708909519f95ff57ea839a367eba8f7d
ElvisAns <ansimapersic@gmail.com>
2022-08-21 18:25:09 +0200

    Fixed some lint errors

--- frontend/src/components/ThePostViewer.vue
+++ frontend/src/components/ThePostViewer.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="container">
         <div class="row">
-            <article class="col-md-10 offset-md-1">
-                
-            </article>
+            <article class="col-md-10 offset-md-1"></article>
         </div>
     </div>
 </template>
 
 <script>
-    export default{
-        name:"ThePostViewer"
-    }
+export default {
+    name: "ThePostViewer",
+};
 </script>
