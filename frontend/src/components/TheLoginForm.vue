86957c68434fa2d9832e3c3001eb04fa7303dc3b
ElvisAns <ansimapersic@gmail.com>
2022-08-21 14:38:33 +0200

    added the logo to login form

--- frontend/src/components/TheLoginForm.vue
+++ frontend/src/components/TheLoginForm.vue
@@ -1,39 +1,119 @@
 <template>
-    <form>
-        <div class="form-outline mb-4">
-            <input type="email" id="form1Example1" class="form-control" />
-            <label class="form-label" for="form1Example1">Email address</label>
-        </div>
-        <div class="form-outline mb-4">
-            <input type="password" id="form1Example2" class="form-control" />
-            <label class="form-label" for="form1Example2">Password</label>
-        </div>
-        <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-                <div class="form-check">
-                    <input
-                        class="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="form1Example3"
-                        checked
-                    />
-                    <label class="form-check-label" for="form1Example3">
-                        Remember me
-                    </label>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 p-4">
+                <img src="@/assets/logo.png" class="logo" />
+                <form class="row g-3 needs-validation mt-5" novalidate>
+                    <div class="col-md-12">
+                        <label for="validationCustomUsername" class="form-label"
+                            >Email</label
+                        >
+                        <div class="input-group has-validation">
+                            <span
+                                class="input-group-text"
+                                id="inputGroupPrepend"
+                                ><i class="bi bi-envelope-fill"></i
+                            ></span>
+                            <input
+                                placeholder="johndoe@gmail.com"
+                                type="text"
+                                class="form-control"
+                                id="validationCustomUsername"
+                                aria-describedby="inputGroupPrepend"
+                                required
+                            />
+                            <div class="invalid-feedback">
+                                {{ error.email }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="validationCustom03" class="form-label"
+                            >City</label
+                        >
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="validationCustom03"
+                            required
+                        />
+                        <div class="invalid-feedback">
+                            Please provide a valid city.
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="validationCustom04" class="form-label"
+                            >State</label
+                        >
+                        <select
+                            class="form-select"
+                            id="validationCustom04"
+                            required
+                        >
+                            <option selected disabled value="">
+                                Choose...
+                            </option>
+                            <option>...</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please select a valid state.
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="validationCustom05" class="form-label"
+                            >Zip</label
+                        >
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="validationCustom05"
+                            required
+                        />
+                        <div class="invalid-feedback">
+                            Please provide a valid zip.
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-check">
+                            <input
+                                class="form-check-input"
+                                type="checkbox"
+                                value=""
+                                id="invalidCheck"
+                                required
+                            />
+                            <label class="form-check-label" for="invalidCheck">
+                                Agree to terms and conditions
+                            </label>
+                            <div class="invalid-feedback">
+                                You must agree before submitting.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <button class="btn btn-primary" type="submit">
+                            Submit form
+                        </button>
+                    </div>
+                </form>
             </div>
-
-            <div class="col">
-                <a href="#!">Forgot password?</a>
-            </div>
         </div>
-        <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-    </form>
+    </div>
 </template>
 
 <script>
 export default {
     name: "TheLoginForm",
+    data() {
+        return {
+            error: {},
+        };
+    },
 };
 </script>
+
+<style scoped>
+.logo {
+    height: 40px;
+}
+</style>
