93e34fd7898f7711a39d63e7f11a4e23bea5ed28
ElvisAns <ansimapersic@gmail.com>
2022-08-21 10:56:03 +0200

    Separation of concern, build the user manager component

---
+++ frontend/src/components/TheUserManager.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex mx-auto justify-content-center">
+        <router-link to="/login" class="btn btn-success mx-1 btn-md px-4">
+            login
+        </router-link>
+        <router-link to="/signin" class="btn btn-light mx-1 btn-md px-4">
+            register
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheUserManager",
+    props: {
+        userLogin: {
+            required: true,
+            type: Boolean,
+        },
+    },
+};
+</script>
