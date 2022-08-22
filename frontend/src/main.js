e50d2f5d8695eb67ba5511ed1c25a154ad6a5afd
ElvisAns <ansimapersic@gmail.com>
2022-08-22 09:12:55 +0200

    Imported Vue cookies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,13 +10,15 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import VueLoading from "vue-loading-overlay";
 import Toast from "vue-toastification";
+import VueCookies from "vue-cookies";
 // Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
-
+axios.defaults.withCredentials = false;
 createApp(App)
     .use(store)
     .use(router)
     .use(VueAxios, axios)
     .use(VueLoading)
     .use(Toast)
+    .use(VueCookies)
     .mount("#app");
