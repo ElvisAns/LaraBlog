77f892538c3351064300a7405a9e9087a8c9f08c
ElvisAns <ansimapersic@gmail.com>
2022-08-22 06:32:15 +0200

    Created the auth  controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    //
+}
