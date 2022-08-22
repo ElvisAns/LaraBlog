1258dbb9670a155d3c06079f6447d7019afd5cd2
ElvisAns <ansimapersic@gmail.com>
2022-08-22 09:57:28 +0200

    Import validator

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 
 class UserController extends Controller
