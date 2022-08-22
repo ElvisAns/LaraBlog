72feda5f1029e69e8fdb694102e3c7cd4a74896e
ElvisAns <ansimapersic@gmail.com>
2022-08-22 04:50:27 +0200

    Some typo

--- frontend/src/components/TheUserProfile.vue
+++ frontend/src/components/TheUserProfile.vue
@@ -10,7 +10,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Are you sure</h5>
+                    <h5 class="modal-title">Are you sure?</h5>
                     <button
                         type="button"
                         class="btn-close"
