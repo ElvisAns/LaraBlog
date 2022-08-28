26a0e9b03540099b7d47dc7abccbc56e37139522
ANSIMA CIBALINDA Elvis <ansimapersic@gmail.com>
2022-08-28 16:31:48 +0200

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8080,::1,https://lara-blog.vercel.app',
+        'localhost:3000,127.0.0.1,127.0.0.1:8080,lara-blog.vercel.app',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
